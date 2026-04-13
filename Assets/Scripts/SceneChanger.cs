dc8bf282288db475936128a9bbec26cd72cfc733
p9805198051 <p9805198051@gmail.com>
2026-04-13 21:50:00 +0800

    Class03:FirstGit
    新專案第四次Git Commit (2026/4/13)

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    // 按下Play時會執行一次(初始化)
+    void Start()
+    {
+      
+    }
+
+    // 畫面每一幀(FPS)執行一次(更新)
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
